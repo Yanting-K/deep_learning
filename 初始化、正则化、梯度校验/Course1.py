f206a7310db58a0dcb60d61bd33ff3bfeafd69bd
kshow <48441641+kytshow@users.noreply.github.com>
2020-03-13 15:33:06 +0800

    Add files via upload

--- 初始化、正则化、梯度校验/Course1.py
+++ 初始化、正则化、梯度校验/Course1.py
@@ -236,7 +236,7 @@ axes.set_ylim([-1.5, 1.5])
 init_utils.plot_moxidecision_boundary(lambda x: init_utils.predict_dec(parameters, x.T), train_X, train_Y)
 '''
 
-
+'''
 #######################正则化模型##########################
 
 train_X, train_Y, test_X, test_Y = reg_utils.load_2D_dataset(is_plot=True)
@@ -327,6 +327,7 @@ def model(X,Y,learning_rate=0.3,num_iterations=30000,print_cost=True,is_plot=Tru
     #返回学习后的参数
     return parameters
 '''
+'''
 #test model
 
 parameters = model(train_X, train_Y,is_plot=True)
@@ -340,7 +341,7 @@ axes = plt.gca()
 axes.set_xlim([-0.75,0.40])
 axes.set_ylim([-0.75,0.65])
 reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
-
+'''
 
 '''
 def compute_cost_with_regularization(A3,Y,parameters,lambd):
@@ -412,6 +413,7 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
 
 
 '''
+'''
 #test regularization#
 parameters = model(train_X, train_Y, lambd=0.7,is_plot=True)
 print("使用正则化，训练集:")
@@ -425,7 +427,7 @@ axes.set_xlim([-0.75,0.40])
 axes.set_ylim([-0.75,0.65])
 reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
 '''
-
+'''
 
 def forward_propagation_with_dropout(X,parameters,keep_prob=0.5):
     """
@@ -555,3 +557,87 @@ axes.set_xlim([-0.75, 0.40])
 axes.set_ylim([-0.75, 0.65])
 reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
 
+'''
+
+########################梯度校验######################
+def forward_propagation_n(X,Y,parameters):
+    """
+    实现图中的前向传播（并计算成本）。
+    
+    参数：
+        X - 训练集为m个例子
+        Y -  m个示例的标签
+        parameters - 包含参数“W1”，“b1”，“W2”，“b2”，“W3”，“b3”的python字典：
+            W1  - 权重矩阵，维度为（5,4）
+            b1  - 偏向量，维度为（5,1）
+            W2  - 权重矩阵，维度为（3,5）
+            b2  - 偏向量，维度为（3,1）
+            W3  - 权重矩阵，维度为（1,3）
+            b3  - 偏向量，维度为（1,1）
+   
+    返回：
+        cost - 成本函数（logistic）
+    """
+    m = X.shape[1]
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1,X) + b1
+    A1 = gc_utils.relu(Z1)
+    
+    Z2 = np.dot(W2,A1) + b2
+    A2 = gc_utils.relu(Z2)
+    
+    Z3 = np.dot(W3,A2) + b3
+    A3 = gc_utils.sigmoid(Z3)
+    
+    #计算成本
+    logprobs = np.multiply(-np.log(A3), Y) + np.multiply(-np.log(1 - A3), 1 - Y)
+    cost = (1 / m) * np.sum(logprobs)
+    
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return cost, cache
+
+def backward_propagation_n(X,Y,cache):
+    """
+    实现图中所示的反向传播。
+    
+    参数：
+        X - 输入数据点（输入节点数量，1）
+        Y - 标签
+        cache - 来自forward_propagation_n（）的cache输出
+    
+    返回：
+        gradients - 一个字典，其中包含与每个参数、激活和激活前变量相关的成本梯度。
+    """
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = (1. / m) * np.dot(dZ3,A2.T)
+    dW3 = 1. / m * np.dot(dZ3, A2.T)
+    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    #dW2 = 1. / m * np.dot(dZ2, A1.T) * 2  # Should not multiply by 2
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
+    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1. / m * np.dot(dZ1, X.T)
+    #db1 = 4. / m * np.sum(dZ1, axis=1, keepdims=True) # Should not multiply by 4
+    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                 "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                 "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+ 
+    return gradients
